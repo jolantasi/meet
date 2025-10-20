a9c7831393cf9c65f4738ca47415cf95baa9a3b5
jolantasi <jolanta.germany@gmail.com>
2025-10-20 10:32:41 +0200

    Added PWA support using vite-plugin-pwa and Workbox libraries.
    Added PWA functionality to the project by installing vite-plugin-pwa and Workbox libraries for caching, routing, and offline support.

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import * as atatus from 'atatus-spa';
-// Initialize Atatus
-atatus.config('dce5390e5b6f43f3bbc17ea5faf88c1f').install();
+//import * as atatus from 'atatus-spa';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+//atatus.config('dce5390e5b6f43f3bbc17ea5faf88c1f').install();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+serviceWorkerRegistration.register();
